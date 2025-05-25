956416a3d94d97a0e3ef6b12ea01752be73d6bc3
naweednri <naweednouri@yahoo.com>
2025-05-25 11:11:18 +0330

    enum IP defined. method parseIP declared.

--- cloudflare.h
+++ cloudflare.h
@@ -12,9 +12,15 @@ public:
     cloudflare(const QString url);
     ~cloudflare();
     bool run();
+    enum class IP
+    {
+        v4,v6
+    };
+
 private:
     QNetworkAccessManager* _manager;
     QString* scrape();
+    std::vector<QString> parseIP(QString* scraped,IP type);
     bool IPsBySubnet(std::string_view rawIPs);
     std::vector<std::string>* _IPv4;
     std::vector<std::string>* _IPv6;
