d72587ae894bd9e3aebbf52fa8bd9bf1b66c05d3
naweednri <naweednouri@yahoo.com>
2025-06-01 17:39:40 +0330

    cloudflare removed. The window is set to a fixed size. The window title is set.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include "mainwindow.h"
-#include "cloudflare.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    cloudflare obj("https://www.cloudflare.com/ips-v4/#");
-    obj.run();
-    //w.show();
+    w.setFixedSize(800,500);
+    w.setWindowTitle("Cloudflare Scanner");
+    w.show();
     return a.exec();
 }
